f7caea3ada0597a7398968cf86aae9e3bf2da111
nikhilsc <74759172+nikhilsc@users.noreply.github.com>
2020-11-20 12:40:28 +0530

    Add files via upload

---
+++ CallByValue.java
@@ -0,0 +1,30 @@
+package o1com.callbyValue;
+
+public class CallByValue {
+	
+	int num = 100;
+	
+	void disp() {
+		System.out.println(num);
+	}
+	
+	void fun(int k) {
+		
+		k = 500;
+		System.out.println("In fun() k: "+k);	// 500
+		System.out.println("In fun() num: "+num); //100
+	}
+	
+	public static void main(String[] args) {
+		
+		CallByValue c1 = new CallByValue();
+		
+		c1.disp();	//100
+		
+		c1.fun(c1.num);
+		
+		c1.disp(); //100
+		
+	}
+
+}
